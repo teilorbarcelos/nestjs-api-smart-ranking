5189e75f2aaae98e7f58e01536d766d8c9362031
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-10-10 23:12:46 -0300

    mongoose added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    PlayersModule,
+    MongooseModule.forRoot(process.env.DB_URL, {
+      useNewUrlParses: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
